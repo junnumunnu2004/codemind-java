830740a4a9f53eecbf33d9fbf5cd8100c61efaea
junnumunnu2004 <121998705+junnumunnu2004@users.noreply.github.com>
2023-06-01 14:11:03 +0530

    import java.util.*; public class code {     public static void main(String args[])     {         Scanner sc= new Scanner(System.in);         char c;         c= sc.nextLine().charAt(0);
        if(c=='V')
        {
           System.out.println("Violet");
        }
        else if(c=='I')
        {
           System.out.println("Indigo");
        }
        else if(c=='B')
        {
             System.out.println("Blue");
        }
        else if(c=='G')
        {
            System.out.println("Green");
            
        }
        else if(c=='Y')
        {
             System.out.println("Yellow");
        }
        else if(c=='O')
        {
             System.out.println("Orange");
        }
        else if(c=='R')
        {
            System.out.println("Red");
        }
        else  if(c=='v')
        {
           System.out.println("Violet");
        }
        else if(c=='i')
        {
           System.out.println("Indigo");
        }
        else if(c=='b')
        {
             System.out.println("Blue");
        }
        else if(c=='g')
        {
            System.out.println("Green");
            
        }
        else if(c=='y')
        {
             System.out.println("Yellow");
        }
        else if(c=='o')
        {
             System.out.println("Orange");
        }
        else if(c=='r')
        {
            System.out.println("Red");
        }
        else
        {
            System.out.println("-1");
        }
        }
    }

---
+++ Color_Code.java
@@ -0,0 +1,74 @@
+import java.util.*;
+public class code
+{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        char c;
+        c= sc.nextLine().charAt(0);
+          
+   
+    if(c=='V')
+    {
+       System.out.println("Violet");
+    }
+    else if(c=='I')
+    {
+       System.out.println("Indigo");
+    }
+    else if(c=='B')
+    {
+         System.out.println("Blue");
+    }
+    else if(c=='G')
+    {
+        System.out.println("Green");
+        
+    }
+    else if(c=='Y')
+    {
+         System.out.println("Yellow");
+    }
+    else if(c=='O')
+    {
+         System.out.println("Orange");
+    }
+    else if(c=='R')
+    {
+        System.out.println("Red");
+    }
+    else  if(c=='v')
+    {
+       System.out.println("Violet");
+    }
+    else if(c=='i')
+    {
+       System.out.println("Indigo");
+    }
+    else if(c=='b')
+    {
+         System.out.println("Blue");
+    }
+    else if(c=='g')
+    {
+        System.out.println("Green");
+        
+    }
+    else if(c=='y')
+    {
+         System.out.println("Yellow");
+    }
+    else if(c=='o')
+    {
+         System.out.println("Orange");
+    }
+    else if(c=='r')
+    {
+        System.out.println("Red");
+    }
+    else
+    {
+        System.out.println("-1");
+    }
+    }
+}
