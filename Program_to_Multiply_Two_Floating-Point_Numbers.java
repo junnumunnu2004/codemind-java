de4bf4a95341c2c2877be35eb1554cfb3740ca58
junnumunnu2004 <121998705+junnumunnu2004@users.noreply.github.com>
2023-06-01 13:23:30 +0530

    import java.util.*; public class multi {     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         double a,b,c;         a=sc.nextDouble();         b=sc.nextDouble();;         c=a*b;         System.out.format("%.2f",c);     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class multi
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        double a,b,c;
+        a=sc.nextDouble();
+        b=sc.nextDouble();;
+        c=a*b;
+        System.out.format("%.2f",c);
+    }
+}
